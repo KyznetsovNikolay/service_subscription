5a3e72c052d200605373d894259badbe740c990c
Nikolay Kyznetsov <kyznetsovnic@gmail.com>
2021-12-06 09:34:32 +0300

    Add User entity

---
+++ src/Module/User/Entity/User.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Module\User\Entity;
+
+use App\Helper\IdTrait;
+use App\Module\User\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="users")
+ */
+class User
+{
+    use IdTrait;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $surname;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function setPrice(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getFullName(): string
+    {
+        return "$this->name $this->surname";
+    }
+}
